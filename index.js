bc8b3706360022f1156aae454627c755905e1ed7
wahome <wahomewgitonga@gmail.com>
2024-03-28 09:07:40 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+  }
+  
+  function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
